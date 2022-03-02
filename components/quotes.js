0637a5755f4f20e5b2395f46b93c0778c191b61c
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-02 15:12:09 +0200

    Everything in Quotes in place

--- components/quotes.js
+++ components/quotes.js
@@ -1,4 +1,47 @@
+import styles from "../styles";
+import database from "../database";
+
 export default function Quotes() {
 
+    // list for saved quotes
+    const [quotelist, setQuotelist] = useState([]);
+
+    // delete a quote
+    const deleteQuote = (key) => {
+        remove(ref(database, 'quotes/' + key)); 
+    };
+
+    // update list
+    useEffect(() => {
+        const listRef = ref(database, 'quotes/');
+        onValue(listRef, (snapshot) => {
+        const data = snapshot.val();
+        const listitems = data ? Object.keys(data).map(key => ({ key, ...data[key] })) : [];
+        setQuotelist(listitems);
+        })
+    }, []);
+
+    return (
+        <View style={styles.container}>
+
+            <StatusBar style="auto" />
+
+            {quotelist.length > 0 ?
+                
+                <FlatList 
+                data={quotelist}
+                keyExtractor={item => item.key}
+                renderItem={({ item }) =>
+                    <View style={styles.listcontainer}>
+                        <Text>{item}</Text>
+                        <Text style={styles.itemtext} onPress={() => deleteQuote(item.key)}>delete</Text>
+                    </View>
+                }        
+                />
+                
+                : <Text>Go to the main page to get inspiring quotes!</Text>}
+
+        </View>
+    );
 
 } 
