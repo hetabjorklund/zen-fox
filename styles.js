1f5df89eaa75b0b99bfe45155834e81b6caf1087
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-02 15:02:30 +0200

    Styles into a separate component, imported elsewhere

---
+++ styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 80,
+      flex: 1,    
+      flexDirection: "column",
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input : {
+      width: 150, 
+      marginTop: 5,
+      marginBottom: 5,
+      borderColor: 'black', 
+      borderWidth: 1
+    },
+    titletext: {
+      marginTop: 10,
+      marginBottom: 5,
+      fontWeight: "bold",
+      color: 'blue'
+    },
+    itemtext: {
+      marginStart: 10,
+      color: 'blue'
+    },
+    listcontainer: {
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-evenly'   
+    }
+});
+
+export default styles;
