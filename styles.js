b65fbf7a73f4a90e8083cb86c85a3630507ba0e8
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-05 15:44:24 +0200

    New styles

--- styles.js
+++ styles.js
@@ -1,49 +1,65 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 80,
+  container: {
       flex: 1,    
       flexDirection: "column",
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
-    },
-    input : {
-      width: 200, 
-      marginTop: 10,
-      marginBottom: 5,
-      borderColor: 'black', 
-      borderWidth: 1
-    },
-    titletext: {
-      marginTop: 10,
-      marginBottom: 5,
-      fontWeight: "bold",
-      color: 'blue'
-    },
-    itemtext: {
-      marginStart: 10,
-      color: 'blue'
-    },
-    listcontainer: {
-      flexDirection: 'row',
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'space-evenly'   
-    },
-    image: { 
-      width: '50%',
-      height: 100,
-      borderRadius: 5
-    },
-    operators: {
-      flexDirection: "row",
-      alignItems: 'center',
-      marginTop: 10,
-      marginBottom: 10,
-      justifyContent: 'space-between'
-    }
+  },
+  innercontainer1: {
+    flex: 1,    
+    flexDirection: "column",
+    backgroundColor: '#e6cd8e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innercontainer2: {
+    flex: 1,    
+    flexDirection: "column",
+    backgroundColor: '#b6e39f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input : {
+    width: 200, 
+    marginTop: 10,
+    marginBottom: 5,
+    borderColor: 'black', 
+    borderWidth: 1
+  },
+  titletext: {
+    marginTop: 10,
+    marginBottom: 5,
+    fontWeight: "bold",
+    color: 'blue'
+  },
+  itemtext: {
+    marginStart: 10,
+    color: 'blue'
+  },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-evenly'   
+  },
+  image: { 
+    width: '50%',
+    height: 100,
+    borderRadius: 5
+  },
+  operators: {
+    flexDirection: "row",
+    alignItems: 'stretch',
+    marginTop: 5,
+    marginBottom: 5,
+    justifyContent: 'space-evenly'
+  },
+  button: {
+
+  }
 });
 
 export default styles;
