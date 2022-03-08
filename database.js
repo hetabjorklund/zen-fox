7a87a71233792bef230fc01cd85c60251e6e9862
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-08 13:35:24 +0200

    Unused imports removed

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 import firebaseConfig from "./FirebaseConfig";
 import { initializeApp } from 'firebase/app';
-import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
+import { getDatabase } from 'firebase/database';
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
